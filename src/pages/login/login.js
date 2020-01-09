13c2d88e8977abd6071caa81e19219bc7dacdf94
yangyuji <394712273@qq.com>
2020-01-09 14:40:20 +0800

    login page

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,8 +1,10 @@
 import './login.scss'
 import $ from 'jQuery'
+import layui from 'layui'
 
 $(function () {
   if(top.location != self.location){
     top.location = self.location //防止页面被框架包含
   }
+  layui.use('form')
 })
